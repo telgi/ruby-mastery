bfe8b58aa0a3c9d7a9acb4c251c8835db4047b2e
Terry Allitt <tallitt88@gmail.com>
2018-01-12 15:01:39 +0000

    Remove unnecessary comments and refactoring

--- hash_group.rb
+++ hash_group.rb
@@ -8,24 +8,14 @@ players = [
   { :name => "Mark", :sport => "football" }
   ]
 
-players_by_sport = {} # create new hash
-
-# create loop to iterate through players and grab value of each :sport key
+players_by_sport = {}
 
 players.each do |player|
   sport = player[:sport]
-
-  # create conditional to convert nil values into empty values
-
   if players_by_sport[sport] == nil
     players_by_sport[sport] = []
   end
-
-    # within same block, push remaining :name values into hash as arrays contained by :sport keys 
-
     players_by_sport[sport].push(player[:name])
 end
-
-# call new hash to print contents
 
 puts players_by_sport
